1f00204dd25ef0aae6275611efd1b9e3e29bce0b
Fernando Wobeto <fernando.wobeto@solucoesglobais.com.br>
2015-01-12 14:56:35 -0200

    changed bootstrap

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,3 +1,7 @@
-<?php 
+<?php
+ini_set('display_errors', true);
+error_reporting(E_ALL);
 
-include  realpath(dirname(__FILE__)) . "/../../../autoload.php";
+$path   = realpath(dirname(__FILE__) . '/../');
+
+$loader = require $path . '/vendor/autoload.php';
